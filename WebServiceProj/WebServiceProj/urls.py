9d5834481c4413a210a9334272c423531bf59969
PVKishan <corpcare@gmail.com>
2024-12-05 22:30:58 +0530

    Webserver basic

--- WebServiceProj/WebServiceProj/urls.py
+++ WebServiceProj/WebServiceProj/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for myproject project.
+URL configuration for WebServiceProj project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/5.1/topics/http/urls/
@@ -16,9 +16,9 @@ Including another URLconf
 """
 
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path,include
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-#    path('',include('pvkapp.urls')),
+    path("admin/", admin.site.urls),
+    path('appweb/',include('appweb.urls')),
 ]
